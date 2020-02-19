e834b97e04585c20da20e14d46bc0f9fb3528669
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-18 21:11:31 -0300

    Nesse primeiro momento entendendo os principios básicos para manipular svg e DOM com d3.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import First from './components/First';
-import Second from './components/Second';
 
 function App() {
   return (
     <>
-      <Second />
+      <First />
     </>
   )
 }
