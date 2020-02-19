14721c0b5f7eee3ca365c2b43ce5fe14c0d17c5a
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-18 21:35:10 -0300

    Estudo de gráficos responsivos.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 //import First from './components/First';
 //import Second from './components/Second';
 //import Third from './components/Third';
-import Fourth from './components/Fourth';
+//import Fourth from './components/Fourth';
+import Sixth from './components/Sixth';
 
 function App() {
   return (
     <>
-      <Fourth/>
+      <Sixth data={data} />
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value < 35))}>
+        Filter data
+      </button>
+      <button
+        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
+      >
+        Add data
+      </button>
     </>
   )
 }
