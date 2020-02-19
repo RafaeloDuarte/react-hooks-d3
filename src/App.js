c38c486fafbc1574835ac13bab74690f52ae5a2d
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-18 21:31:57 -0300

    Estudo de gráficos de barras.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //import First from './components/First';
 //import Second from './components/Second';
-import Third from './components/Third';
+//import Third from './components/Third';
+import Fourth from './components/Fourth';
 
 function App() {
   return (
     <>
-      <Third/>
+      <Fourth/>
     </>
   )
 }
