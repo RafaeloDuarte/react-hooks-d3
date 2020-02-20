8db3b3e8784ff10dcf3c490b55d43232e1614905
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-19 21:56:50 -0300

    Primeira fase da impelemntação de zoomable-sunburt com react hooks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+
 //import First from './components/First';
 //import Second from './components/Second';
 //import Third from './components/Third';
 //import Fourth from './components/Fourth';
-import Sixth from './components/Sixth';
+//import Sixth from './components/Sixth';
+import ChartComponent from './components/ChartComponent';
 
 function App() {
+
   return (
     <>
-      <Sixth data={data} />
-      <button onClick={() => setData(data.map(value => value + 5))}>
-        Update data
-      </button>
-      <button onClick={() => setData(data.filter(value => value < 35))}>
-        Filter data
-      </button>
-      <button
-        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
-      >
-        Add data
-      </button>
+      <ChartComponent/>
     </>
   )
 }
